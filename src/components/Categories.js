a0debcef8f94b6923ae8c1fd931e98e7cf5685c5
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-04-22 21:38:57 +0200

    Edit grid style in job categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
-import items from './JobsData';
+import React, { useState } from "react";
+import items from "./JobsData";
 
 const Categories = ({ categories, filterItem }) => {
+  const [activeClass, setActiveClass] = useState("");
 
-    const [activeClass, setActiveClass] = useState('');
+  const activeFn = () => {
+    return setActiveClass("active");
+  };
 
-    const activeFn = () => {
-       return setActiveClass('active');
-    }
-
-    return (
-        <section>
-          <p style={{marginBottom : "10px"}}>we have <span style={{ color : " blue" }}>{items.length}</span> potential jobs for you.</p>
-            <div className="btn-container">
-                {categories.map((jobType, index) => {
-                    return ( 
-                        <button type="button" className={`filter-category ${index === 0 && activeClass}`} key={index} onClick={() => {filterItem(jobType);
-                        activeFn();}}>{jobType}</button>
-                    )
-                })}
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <p style={{ marginBottom: "10px" }}>
+        we have <span style={{ color: " blue" }}>{items.length}</span> potential
+        jobs for you.
+      </p>
+      <div className="btn-container">
+        {categories.map((jobType, index) => {
+          return (
+            <button
+              type="button"
+              className={`filter-category`}
+              key={index}
+              onClick={() => {
+                filterItem(jobType);
+                // activeFn();
+              }}
+            >
+              {jobType}
+            </button>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
 export default Categories;
